6ac6d8c9f035d046c1554a22aec80718f2424e4c
xygodcyx <109750146+xygodcyx@users.noreply.github.com>
2024-12-06 19:26:54 +0800

    update

--- index.cjs
+++ index.cjs
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.get("/locale", async (req, res) => {
     const {ip} = req.query;
